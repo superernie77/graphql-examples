4bcfaa5f0793e85b8ffac3606040165a5ccdb6e3
superernie77 <superernie77@gmail.com>
2021-04-17 16:40:15 +0200

    Job-Company relation

---
+++ job-board/client/src/requests.js
@@ -0,0 +1,26 @@
+const endpointUrl = 'http:localhost:9000/graphql';
+
+export async function loadJobs(){
+    const response = await fetch(endpointURL, {
+        method : 'POST',
+        header : {'content-type' : 'application/json'},
+        body : JSON.stringify({
+            query: `{
+                jobs 
+                {
+                  id 
+                  title
+                  company {
+                    id
+                    name
+                  }   
+                  }
+              }
+            
+            `
+        })
+    });
+    const responseBody = await response.json();
+    return responseBody.data.jobs;
+
+}
--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -4,5 +4,9 @@ const Query = {
     jobs : () => db.jobs.list()
 };
 
+const Job = {
+    company : (job) => db.companies.get(job.companyId)
+}
 
-module.exports = {Query}
+
+module.exports = {Query, Job}
