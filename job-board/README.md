3d78e61a268b31000d1f80e56a45ae900363ad89
superernie77 <superernie77@gmail.com>
2021-05-07 13:54:01 +0200

    improved mutation

--- job-board/README.md
+++ job-board/README.md
@@ -1,3 +1,5 @@
 # GraphQL Job Board
 
 Sample application used in the GraphQL by Example course.
+
+Test GUI: http://localhost:9000/graphql
--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -7,8 +7,9 @@ const Query = {
 };
 
 const Mutation = {
-    createJob: (root, {company,title,description}) => {
-        return db.jobs.create({company,title,description});
+    createJob: (root, {input}) => {
+        const id =  db.jobs.create(input);
+        return db.jobs.get(id);
     }
 }
 
