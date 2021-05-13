283f337faa717df6d1e1c87c5dd4c56337f2067a
superernie77 <superernie77@gmail.com>
2021-05-13 13:02:30 +0200

    passing auth token to server

--- job-board/client/src/requests.js
+++ job-board/client/src/requests.js
@@ -1,10 +1,17 @@
+import {getAccessToken, isLoggedIn } from './auth';
 const endpointUrl = 'http:localhost:9000/graphql';
 
 async function graphQlRequest(query, variables = {}){
-  const response = await fetch(endpointURL, {
-      method : 'POST',
-      header : {'content-type' : 'application/json'},
-      body : JSON.stringify({query, variables})});
+  const request = {
+    method : 'POST',
+    header : {'content-type' : 'application/json'},
+    body : JSON.stringify({query, variables})
+  };
+  if (isLoggedIn()){
+    request.headers['authorization'] = 'Bearer ' + getAccessToken();    
+  }
+
+  const response = await fetch(endpointURL, request);
   const responseBody = await response.json();
   if(responseBody.errors){
     const message = responseBody.errors.map((error) => error.message).join('\n');
